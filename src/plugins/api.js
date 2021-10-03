00ef281e8b2833e027d3c05875d97aa41e91f12a
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2021-10-03 14:39:09 -0300

    api config refactor

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,8 +1,44 @@
 import axios from 'axios'
 
-axios.interceptors.request.use(config => {
-  config.baseURL = 'http://localhost:5000/'
-  return config
-})
+const baseAPI = axios.create()
 
-export default axios
+const token = null
+const apiUrl = 'http://localhost:5000/'
+
+baseAPI.interceptors.request.use(
+  function handleRequest (config) {
+    if (apiUrl) config.baseURL = apiUrl
+    if (token) config.headers.Authorization = 'Bearer ' + token
+    return config
+  },
+  function handleError (error) {
+    return Promise.reject(error)
+  }
+)
+
+baseAPI.interceptors.response.use(
+  function handleResponse (response) {
+    return response
+  },
+  function handleError (error) {
+    if (error && error?.response?.data?.detail) {
+      console.error('❌ ' + error.response.data.detail)
+    }
+    if (error && error.response) {
+      switch (error.response.status) {
+        case 401:
+          console.error('Você não possui permissão para acessar.')
+          break
+        default:
+          console.error(error.response.data.Message)
+          break
+      }
+    } else {
+      console.error(error)
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+export default baseAPI
